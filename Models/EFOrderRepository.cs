f9bac733507ec15a73efb3763f53aee91ca7d6ef
aminfarahani <aminfarahani6@gmail.com>
2020-05-23 23:04:52 +0430

    git status

---
+++ Models/EFOrderRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class EFOrderRepository : IOrderRepository
+    {
+        private readonly ApplicationDbContext _ctx;
+
+        public EFOrderRepository(ApplicationDbContext ctx)
+        {
+            this._ctx = ctx;
+        }
+        public IQueryable<Order> Orders => _ctx.Orders
+            .Include(o => o.Lines)
+            .ThenInclude(l => l.Product);
+
+        public void SaveOrder(Order order)
+        {
+            _ctx.AttachRange(order.Lines.Select(l => l.Product));
+            if (order.OrderID == 0)
+                _ctx.Orders.Add(order);
+            _ctx.SaveChanges();
+        }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class Order
+    {
+        [BindNever]
+        public int OrderID { get; set; }
+        [BindNever]
+        public ICollection<CartLine> Lines { get; set; }
+
+        [Required(ErrorMessage ="Please Enter Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Please Enter First Address Line")]
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string Line3 { get; set; }
+
+        [Required(ErrorMessage =("Please Enter a Ciry Name"))]
+        public string City { get; set; }
+        [Required(ErrorMessage = ("Please Enter a State Name"))]
+        public string State { get; set; }
+        public int Zip { get; set; }
+        [Required(ErrorMessage = ("Please Enter a Country Name"))]
+        public string Country { get; set; }
+
+        public bool GiftWrap { get; set; }
+
+
+    }
+}
