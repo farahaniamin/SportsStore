ee58622182faf4c356a98217862e14815e341167
aminfarahani <aminfarahani6@gmail.com>
2020-05-25 02:09:53 +0430

    Admin Page Add

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IProductRepository repository;
+
+        public AdminController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+        public IActionResult index() => View(repository.Products);
+
+        public IActionResult Edit(int productId) =>
+            View(repository.Products.FirstOrDefault(p => p.ProductID == productId));
+
+        public IActionResult Edit (Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveProduct(product);
+                TempData["message"] = $"{product.Name} Has Been Saved.";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+    }
+}
--- Models/EFProductRepository.cs
+++ Models/EFProductRepository.cs
@@ -14,5 +14,25 @@ namespace SportsStore.Models
             this.context = ctx;
         }
         public IQueryable<Product> Products => context.Products;
+
+        public void SaveProduct(Product product)
+        {
+            if(product.ProductID == 0)
+            {
+                context.Products.Add(product);
+            }
+            else
+            {
+                Product dbEntry = context.Products.FirstOrDefault(p => p.ProductID == product.ProductID);
+                if(dbEntry != null)
+                {
+                    dbEntry.Name = product.Name;
+                    dbEntry.Price = product.Price;
+                    dbEntry.Discription = product.Discription;
+                    dbEntry.Category = product.Category;
+                }
+            }
+            context.SaveChanges();
+        }
     }
 }
